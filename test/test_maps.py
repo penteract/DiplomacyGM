9c3cb2567d2d238e53c3f2b2edb65147af87789c
Rua Fuller <ruafuller@gmail.com>
2026-01-16 22:15:01 +0000

    Procrastinate doing important stuff

--- test/test_maps.py
+++ test/test_maps.py
@@ -7,24 +7,30 @@ from DiploGM.models.unit import UnitType
 from test.utils import BoardBuilder
 
 class TestMaps(unittest.TestCase):        
-    def test_1(self):
-        print("<h1>TEST 1</h1>")
+    def test_1_create_board(self):
+        print("<h1>CREATE BOARD</h1>")
         b = BoardBuilder()
 
         b.output()       
 
-    def test_2(self):
-        print("<h1>TEST 2</h1>")
+    def test_2_basic_moves(self):
+        print("<h1>BASIC MOVES</h1>")
         b = BoardBuilder()
 
         b.build(b.germany, (UnitType.FLEET, "Kiel"))
         b.build(b.germany, (UnitType.ARMY, "Mun"))
+        b.build(b.germany, (UnitType.ARMY, "Ber"))
         b.builds_adjudicate(self)
         b.output()
 
-        f_kiel = b.move(b.germany, UnitType.FLEET, "Kiel", "Berlin")
-        b.supportMove(b.germany, UnitType.ARMY, "Munich", f_kiel, "Berlin")
+        b.move(b.germany, UnitType.ARMY, "Ber", "Pru")
+        f_kiel = b.move(b.germany, UnitType.FLEET, "Kie", "Ber")
+        b.supportMove(b.germany, UnitType.ARMY, "Mun", f_kiel, "Ber")
         b.output()
 
         b.moves_adjudicate(self)
+
+        b.move(b.germany, UnitType.FLEET, "Ber", "BAL")
+        b.move(b.germany, UnitType.ARMY, "Pru", "War")
+        b.move(b.germany, UnitType.ARMY, "Mun", "Tyr")
         b.output()
